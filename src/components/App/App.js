2082d623b73addfdbcc2e1ac92666d00d758de8e
SarNadya <lutik.ne@gmail.com>
2022-06-09 00:15:08 +0300

    добавила выделение активной ссылки в React-Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,30 +4,32 @@ import Todo from '../Todo/Todo';
 import About from '../About/About';
 import styles from './App.module.css';
 
-const setActive = ({isActive}) => isActive? "active_link" : '';
+const setActive = ({isActive}) => {
+	const linkStyles = [styles.link];
+	if (isActive) linkStyles.push(styles.active_link);
+	return linkStyles.join(' ');
+};
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className={styles.sidebar}>
-					<nav className={styles.menu}>
+					<div className={styles.menu}>
 						<NavLink
 							to='/'
 							exact = 'true'
-							className={styles.link}
-							// activeClassName={styles.active_link}
+							className={setActive}
 						>
 							Дела
 						</NavLink>
 						<NavLink
 							to='/about'
-							className={styles.link}
-							// activeClassName={styles.active_link}
+							className={setActive}
 						>
 							Обо мне
 						</NavLink>
-					</nav>
+					</div>
 					<Routes className={styles.content}>
 						<Route exact = 'true' path='/' element={<Todo/>}/>
 						<Route path='/about' element={<About/>}/>
